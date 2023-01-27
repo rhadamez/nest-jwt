227a6ac1c6e77095b5d2c48a1fbb013eeb042ca1
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2023-01-27 14:37:17 -0300

    feat: add jwt and refresh token return

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { AtStrategy, RtStrategy } from './strategies';
 
-@Module({})
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService, AtStrategy, RtStrategy],
+  imports: [PrismaModule, JwtModule.register({})],
+})
 export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import { Tokens } from './dto/types';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'at-secret',
+          expiresIn: 10,
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'rt-secret',
+          expiresIn: 3600,
+        },
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
+  }
+
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
+    const password = await this.hashData(dto.password);
+    const user = await this.prismaService.user.create({
+      data: {
+        email: dto.email,
+        password,
+      },
+    });
+
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+
+    return tokens;
+  }
+
+  async updateRtHash(userId: number, rt: string) {
+    const hash = await this.hashData(rt);
+    await this.prismaService.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashRt: hash,
+      },
+    });
+  }
+
+  signinLocal() {}
+  logout() {}
+  refreshTokens() {}
+}
